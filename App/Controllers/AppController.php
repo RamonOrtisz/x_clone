8dffb9100bc1b1d3c79d6e2ce572827d67dce728
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2025-02-04 18:59:20 -0300

    exibindo dados no perfil do usuario

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -19,6 +19,16 @@ class AppController extends Action{
         $tweets = $tweet->getAll();
 
         $this->view->tweets = $tweets;
+
+
+        $usuario = Container::getModel('Usuario');
+
+        $usuario->__set('id', $_SESSION['id']);
+
+        $this->view->info_usuario = $usuario->getInfoUsuario();
+        $this->view->total_tweets = $usuario->getTotalTweets();
+        $this->view->total_seguindo = $usuario->getTotalSeguindo();
+        $this->view->total_seguidores = $usuario->getTotalSeguidores();
             
         $this->render('timeline');
         
